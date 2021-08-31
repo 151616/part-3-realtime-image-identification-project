e331b8945ba483f935e047fa512b3e533f1b38fd
shivansh payak <75551006+151616@users.noreply.github.com>
2021-08-30 21:43:33 -0400

    Add files via upload

---
+++ canvas-page.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas = createCanvas(1000,550);
+    canvas.center();
+    
+}
+function draw(){
+
+}
+function back(){
+    window.location = "index.html"
+}
---
+++ main.js
@@ -0,0 +1,10 @@
+function setup(){
+    canvas = createCanvas(300,250);
+    canvas.center();
+}
+function draw(){
+
+}
+function full_screen_canvas(){
+    window.location = "canvas-page.html";
+}
